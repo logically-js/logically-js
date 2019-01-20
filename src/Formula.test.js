9b881a437e9629cce34b1155fe416db850a34662
Jesse Rappaport <jessetherappaport@gmail.com>
2019-01-20 18:38:20 -0500

    Separate positive and negative test cases

--- src/Formula.test.js
+++ src/Formula.test.js
@@ -3,33 +3,51 @@ import { assert } from 'chai';
 import Formula from './Formula';
 
 describe('Formula', function() {
-  it('should exist', function() {
+  it('should be imported correctly', function() {
     const formula = new Formula();
     assert.exists(formula);
   });
-  describe('findMainBinaryOperatorIndex', function() {
-    const testCases = [
-      { formula: 'p & q', result: 2 },
-      { formula: 'p -> q', result: 2 },
-      { formula: '(p & q) & r', result: 8 },
-      { formula: '(p V q) -> (p & q)', result: 8 },
-      { formula: 'p <-> q & r', result: 2 }, // example of right-associativity
-      /** Negative cases. */
-      { formula: '~p', result: -1 }, // main operator is unary (not binary)
-      { formula: '~(p V q)', result: -1 },
-      { formula: 'p', result: -1 }, // atomic formula
-      { formula: 'foo', result: -1 } // non-wff
-    ];
-    for (const test of testCases) {
-      it(`should return ${test.result} for the formula '${
-        test.formula
-      }'`, function() {
-        const formula = new Formula();
-        assert.equal(
-          formula.findMainBinaryOperatorIndex(test.formula),
-          test.result
-        );
-      });
-    }
+  describe('findMainBinaryOperatorIndex()', function() {
+    describe('is correct for positive test cases', function() {
+      const testCases = [
+        { formula: 'p & q', result: 2 },
+        { formula: 'p -> q', result: 2 },
+        { formula: '(p & q) & r', result: 8 },
+        { formula: '(p V q) -> (p & q)', result: 8 },
+        { formula: '(p V q) -> (p & q)', result: 8 },
+        { formula: 'p <-> q & r', result: 2 } // example of right-associativity
+      ];
+      for (const test of testCases) {
+        it(`should return ${test.result} for the formula '${
+          test.formula
+        }'`, function() {
+          const formula = new Formula();
+          assert.equal(
+            formula.findMainBinaryOperatorIndex(test.formula),
+            test.result
+          );
+        });
+      }
+    });
+
+    describe('is correct for negative test cases', function() {
+      const testCases = [
+        { formula: '~p', result: -1 }, // main operator is unary (not binary)
+        { formula: '~(p V q)', result: -1 },
+        { formula: 'p', result: -1 }, // atomic formula
+        { formula: 'foo', result: -1 } // non-wff
+      ];
+      for (const test of testCases) {
+        it(`should return ${test.result} for the formula '${
+          test.formula
+        }'`, function() {
+          const formula = new Formula();
+          assert.equal(
+            formula.findMainBinaryOperatorIndex(test.formula),
+            test.result
+          );
+        });
+      }
+    });
   });
 });
