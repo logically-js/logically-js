1cde5eb02c487bef08f9561af2288719c20cbc3f
Jesse Rappaport <jessetherappaport@gmail.com>
2019-01-20 17:53:57 -0500

    Add some tests for findMainBinaryOperatorIndex

--- src/Formula.test.js
+++ src/Formula.test.js
@@ -4,7 +4,31 @@ const Formula = require('./Formula.js').default;
 
 describe('Formula', function() {
   it('should exist', function() {
-    const formula = new Formula('p');
+    const formula = new Formula();
     assert.exists(formula);
+  });
+  describe('findMainBinaryOperatorIndex', function() {
+    it('should return 2 for the formula `p & q` ', function() {
+      const formula = new Formula();
+      assert.equal(formula.findMainBinaryOperatorIndex('p & q'), 2);
+    });
+
+    it('should return 2 for the formula `p -> q`', function() {
+      const formula = new Formula();
+      assert.equal(formula.findMainBinaryOperatorIndex('p -> q'), 2);
+    });
+
+    it('should return 8 for the formula `(p & q) & r`', function() {
+      const formula = new Formula();
+      assert.equal(formula.findMainBinaryOperatorIndex('(p & q) & r'), 8);
+    });
+
+    it('should return 8 for the formula `(p V q) -> (p & q)`', function() {
+      const formula = new Formula();
+      assert.equal(
+        formula.findMainBinaryOperatorIndex('(p V q) -> (p & q)'),
+        8
+      );
+    });
   });
 });
