9865e351b7b4bf457bf18c20d475e58486898082
Jesse Rappaport <jessetherappaport@gmail.com>
2019-07-12 08:21:20 -0400

    Docs for DS

--- src/deductionFunctions/disjunctive-syllogism/index.ts
+++ src/deductionFunctions/disjunctive-syllogism/index.ts
@@ -2,6 +2,19 @@
 import { DeductionRuleInterface } from '../index';
 /* eslint-enable no-unused-vars */
 
+/**
+ * Checks for the valid application of `Disjunctive Syllogism`.
+ *
+ * E.g.: (1) p V q; (2) ~p; (3) q;
+ *
+ * We first identify which of the cited lines is the disjunction by checking
+ * which one is longer, since the disjunction must be longer than the
+ * negation. (Proof: The disjuntion contains the same main argument as the
+ * negation, minus the `~` symbol, plus `V` and another formula.)
+ * Once we have done this, we simply identify whether one of operands
+ * of the disjunction is the negation of the other cited line, and the
+ * other operand is equal to the `target` proposition.
+ */
 export const disjunctiveSyllogism: DeductionRuleInterface = (
   target,
   sources
