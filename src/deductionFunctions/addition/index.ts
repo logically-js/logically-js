afc242c779609bb9e62c908f9d12465198ca856e
Jesse Rappaport <jessetherappaport@gmail.com>
2019-07-11 23:15:26 -0400

    Docs for Associativity

--- src/deductionFunctions/addition/index.ts
+++ src/deductionFunctions/addition/index.ts
@@ -1,6 +1,17 @@
-/* eslint-disable-next-line */
+/* eslint-disable no-unused-vars */
 import { DeductionRuleInterface } from '../index';
+import { Operator } from '../../constants';
 
+/**
+ * Function to identify an application of the rule of `Addition`.
+ * Consider a basic case - source: p; target: p V q;
+ * We can verify that this is a valid application of `Addition` simply
+ * by checking whether any of the 0-2 `target.proposition.operands` is
+ * equal to the `source.proposition`, and that the [[Operator]] for the `target` is `V`
+ *
+ * @param target - The later, target proposition.
+ * @return -
+ */
 export const addition: DeductionRuleInterface = (target, sources) =>
   target.proposition.operands.some(operand =>
     operand.isEqual(sources[0].proposition)
--- src/deductionFunctions/associativity/index.ts
+++ src/deductionFunctions/associativity/index.ts
@@ -9,8 +9,16 @@ import { Formula } from '../../Formula';
 /**
  * Checks for the application of Associativity at the top level.
  *
- * @param {Formula} t - target proposition
- * @param {Formula} s - source proposition
+ * (We call these the target and the source, but because rules of
+ * replacement apply in both directions, we could use the same rule
+ * to go from the target to the source as we use to go from the source
+ * to the target.)
+ *
+ * p V (q V r) ==> (p V q) V r ==> p V (q V r)
+ * (p & q) & r ==> p & (q & r) ==> (p & q) & r
+ *
+ * @param {Formula} t - Target proposition.
+ * @param {Formula} s - Source proposition.
  * @return {boolean} - Can you go from target to source with Associativity?
  */
 const simpleAssociativity: SimpleDeductionRuleInterface = (t, s) => {
@@ -18,9 +26,23 @@ const simpleAssociativity: SimpleDeductionRuleInterface = (t, s) => {
   if (!(op.match(/[&V]/) && op === s.operator)) {
     return false;
   }
-  // Try associating to one side
+  // Try associating to one side (p V q) V r
   let operandFormula = t.operands[0];
   if (operandFormula.operator === op) {
+    // The inner operator has to match the main operator.
+    //
+    // Now we simply create a new formula by accessing the operands of the
+    // disjunctions, e.g., of `t` and moving the parentheses around
+    // and then we pass this to the [[Formula.constructor]].
+    // Here we end up with a single formula on the left and a grouping of
+    // two formulas joined together on the right side, with the correct
+    // operator.
+    // Once this new [[Formula]] has been constructed, we can use
+    // the [[Formula.isEqual]] method to compare it to `s`.
+    // If it is equal, then have shown that you can transform the
+    // `target` via the relevant rule, Associativity, into a formula
+    // that is equal to the `source`, which shows that going from `source`
+    // to `target` or vice versa is a valid move.
     const newFormula = new Formula(
       `(${operandFormula.operands[0].cleansedFormulaString}) ${op} (${operandFormula.operands[1].cleansedFormulaString} ${op} ${t.operands[1].cleansedFormulaString})`
     );
@@ -29,7 +51,10 @@ const simpleAssociativity: SimpleDeductionRuleInterface = (t, s) => {
     }
   }
 
-  // Try associating to the other direction
+  // Try next on the other side.
+  // Construct a formula that has two formulas attached on the left
+  // side, and then joined with a single formula on the right side by the
+  // same operator.
   operandFormula = t.operands[1];
   if (operandFormula.operator === op) {
     const newFormula = new Formula(
@@ -40,6 +65,12 @@ const simpleAssociativity: SimpleDeductionRuleInterface = (t, s) => {
     }
     return false;
   }
+  // Both attempts to apply the rule have failed. Unfortunately, we cannot
+  // conclude that the move was invalid because we only looked at the
+  // top level arguments. But we can conclude that you cannot go from
+  // `target` to `source` using `Associativity` at the top level.
+  // With this simple level of associativity, we use the higher-order
+  // component [[checkRuleRecursively]].
   return false;
 };
 
