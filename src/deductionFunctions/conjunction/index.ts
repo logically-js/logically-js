68f92cd445156d2ca1b231faea0e3e1827fd46db
Jesse Rappaport <jessetherappaport@gmail.com>
2019-07-12 01:17:54 -0400

    Add docs for Constructive Dilemma

--- src/deductionFunctions/conjunction/index.ts
+++ src/deductionFunctions/conjunction/index.ts
@@ -1,6 +1,31 @@
 /* eslint-disable-next-line */
 import { DeductionRuleInterface } from '../index';
 
+/**
+ * `Conjunction` is another rather trivial rule to identify,
+ * and it can only apply at the top level.
+ * A Formula that is derived via `Conjunction` is a conjunction
+ * and each operand has to match one of the cited lines.
+ *
+ * @note: This is a slightly permissive version of `conjunction`.
+ * Classically, we would expect it to accept a proof like:
+ * (1) p V r; (2) q; (3) (p V r) & q
+ * However, it also accepts the following: (1) p; (2) q; (3) p & p
+ * It isn't clear to me whether this behavior is good or bad.
+ * On the plus side it gives a straightforward deduction for:
+ * (1) q; (2) q & q; (Although this is already covered by `Tautology`.)
+ * *Actually*, it would really depend on the number of cited lines.
+ * If there is only one cited line, we might opt to allow the following:
+ * (1) p PREMISE []
+ * (2) p & p CONJUNCTION [1]
+ * But the following would be invalid:
+ * (1) p PREMISE []
+ * (2) q PREMISE []
+ * (3) p & p CONJUNCTION [1, 2]
+ *
+ * In any case, it's probably best to enforce a more strict version of
+ * this function. TODO: See above.
+ */
 export const conjunction: DeductionRuleInterface = (target, sources) =>
   target.proposition.operands.some(operand =>
     operand.isEqual(sources[0].proposition)
--- src/deductionFunctions/constructive-dilemma/index.ts
+++ src/deductionFunctions/constructive-dilemma/index.ts
@@ -1,16 +1,28 @@
 /* eslint-disable-next-line */
 import { DeductionRuleInterface } from '../index';
 
+/**
+ * Function for checking the valid application of Constructive Dilemma.
+ *
+ * Constructive Dilemma has a very particular structure, so we just need
+ * to identify this structure and then compare the appropriate arguments.
+ * Basically, CD consists of a disjunction, and a conjunction of conditionals,
+ * as in: (1) p V q; (2) (p -> r) & (q -> s); (3) r V s;
+ */
 export const constructiveDilemma: DeductionRuleInterface = (
   target,
   sources
 ) => {
+  // First, we identify the conjunction cited line and the disjunction.
+  // The conjunction will necessarilly be longer than the disjunction
+  // (see (2) vs. (1) abve). Proof: The conjunction contains the same variables
+  // as the disjunction, plus two more formulas, plus two more
+  // operators as well.
   const [conj, disj] =
     sources[0].proposition.cleansedFormulaString.length >
     sources[1].proposition.cleansedFormulaString.length
       ? [sources[0].proposition, sources[1].proposition]
       : [sources[1].proposition, sources[0].proposition];
-  console.log(disj.cleansedFormulaString, conj.cleansedFormulaString);
   if (
     target.proposition.operator !== 'V' ||
     conj.operator !== '&' ||
