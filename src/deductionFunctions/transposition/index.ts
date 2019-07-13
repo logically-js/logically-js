b3d4d819e1ff2fe7630ad3b6886d8b82e2b936f6
Jesse Rappaport <jessetherappaport@gmail.com>
2019-07-13 01:20:36 -0400

    Docs for Transposition

--- src/deductionFunctions/transposition/index.ts
+++ src/deductionFunctions/transposition/index.ts
@@ -6,12 +6,21 @@ import {
 } from '../index';
 /* eslint-enable no-unused-vars */
 
+/**
+ * Transposition involves "flipping the arguments" of a conditional around, as
+ * well as "inverting their sign," i.e., negating them. So, to verify
+ * this rule at the top level, we just check that both propositions are
+ * conditionals,
+ */
 const simpleTransposition: SimpleDeductionRuleInterface = (t, s) =>
   t.operator === '->' &&
   s.operator === '->' &&
   t.operands[0].isNegation(s.operands[1]) &&
   t.operands[1].isNegation(s.operands[0]);
 
+/**
+ * Check for applications of Transposition recursively.
+ */
 export const transposition: DeductionRuleInterface = (target, sources) =>
   checkRuleRecursively(simpleTransposition)(
     target.proposition,
