7a5598d0be15057fa89084f3270cb69f81237222
Jesse Rappaport <jessetherappaport@gmail.com>
2019-07-13 08:35:52 -0400

    Docs for Modus Tollens

--- src/deductionFunctions/modus-tollens/index.ts
+++ src/deductionFunctions/modus-tollens/index.ts
@@ -1,6 +1,13 @@
 /* eslint-disable-next-line */
 import { DeductionRuleInterface } from '../index';
 
+/**
+ * Function that checks for the application of Modus Tollens:
+ * (i) ~q; (ii) p -> q; (iii) ~p
+ * This follows the same logic as [[modusPonens]], except that here we go from
+ * the negation of the consequent of a conditional to the negation of the
+ * antecedent.
+ */
 export const modusTollens: DeductionRuleInterface = (target, sources) => {
   const [longer, shorter] =
     sources[0].proposition.cleansedFormulaString.length >
